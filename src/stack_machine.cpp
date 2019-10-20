ce31f10e06e0a9789cffc1bba99f54fdf62a4b23
Yaroslav Riabtsev <44510108+yaryabtsev@users.noreply.github.com>
2019-10-20 21:44:01 +0300

    Update stack_machine.cpp

--- src/stack_machine.cpp
+++ src/stack_machine.cpp
@@ -30,62 +30,62 @@ namespace xi {
 //==============================================================================
 
 
-    int PlusOp::operation(char op, int a, int b,
-                          int /*c*/) // < just commented unused argument. This does not affect the code anyhow.
-    {
-        if (op != '+')
-            throw std::logic_error("Operation other than Plus (+) are not supported");
+int PlusOp::operation(char op, int a, int b,
+                      int /*c*/) // < just commented unused argument. This does not affect the code anyhow.
+{
+    if (op != '+')
+        throw std::logic_error("Operation other than Plus (+) are not supported");
 
-        // here we just ignore unused operands
-        return a + b;
-    }
+    // here we just ignore unused operands
+    return a + b;
+}
 
-    IOperation::Arity PlusOp::getArity() const
-    {
-        return arDue;
-    }
+IOperation::Arity PlusOp::getArity() const
+{
+    return arDue;
+}
 
-    int MultOp::operation(char op, int a, int b, int /*c*/)
-    {
-        if (op != '*')
-            throw std::logic_error("Operation other than Mult (*) are not supported");
+int MultOp::operation(char op, int a, int b, int /*c*/)
+{
+    if (op != '*')
+        throw std::logic_error("Operation other than Mult (*) are not supported");
 
-        return a * b;
-    }
+    return a * b;
+}
 
-    IOperation::Arity MultOp::getArity() const
-    {
-        return arDue;
-    }
+IOperation::Arity MultOp::getArity() const
+{
+    return arDue;
+}
 
-    int OrOp::operation(char op, int a, int b, int /*c*/)
-    {
-        if (op != '|')
-            throw std::logic_error("Operation other than Or (|) are not supported");
+int OrOp::operation(char op, int a, int b, int /*c*/)
+{
+    if (op != '|')
+        throw std::logic_error("Operation other than Or (|) are not supported");
 
-        return a | b;
-    }
+    return a | b;
+}
 
-    IOperation::Arity OrOp::getArity() const
-    {
-        return arDue;
-    }
+IOperation::Arity OrOp::getArity() const
+{
+    return arDue;
+}
 
-    int DivOp::operation(char op, int a, int b, int /*c*/)
-    {
-        if (op != '/')
-            throw std::logic_error("Operation other than Div (/) are not supported");
+int DivOp::operation(char op, int a, int b, int /*c*/)
+{
+    if (op != '/')
+        throw std::logic_error("Operation other than Div (/) are not supported");
 
-        if (!b)
-            return a < 0 ? -INT_MAX : INT_MAX;
+    if (!b)
+        return a < 0 ? -INT_MAX : INT_MAX;
 
-        return a / b;
-    }
+    return a / b;
+}
 
-    IOperation::Arity DivOp::getArity() const
-    {
-        return arDue;
-    }
+IOperation::Arity DivOp::getArity() const
+{
+    return arDue;
+}
 
 //==============================================================================
 // class StackMachine
@@ -94,165 +94,165 @@ namespace xi {
 
 // TODO: put StackMachine methods implementation here
 
-    void StackMachine::registerOperation(char symb, IOperation* oper)
-    {
-        SymbolToOperMapConstIter it = _opers.find(symb);
-        if (it != _opers.end())
-            throw std::logic_error("An operation is already registered");
+void StackMachine::registerOperation(char symb, IOperation* oper)
+{
+    SymbolToOperMapConstIter it = _opers.find(symb);
+    if (it != _opers.end())
+        throw std::logic_error("An operation is already registered");
 
-        _opers[symb] = oper;
-    }
+    _opers[symb] = oper;
+}
 
-    IOperation *StackMachine::getOperation(char symb)
-    {
-        SymbolToOperMapConstIter it = _opers.find(symb);
-        if (it == _opers.end())
-            throw std::logic_error("An operation isn't registered");
+IOperation* StackMachine::getOperation(char symb)
+{
+    SymbolToOperMapConstIter it = _opers.find(symb);
+    if (it == _opers.end())
+        throw std::logic_error("An operation isn't registered");
 
-        return it->second;
-    }
+    return it->second;
+}
 
-    int StackMachine::calculate(const std::string& expr, bool clearStack)
-    {
-        if(clearStack)
-            _s.clear();
-        std::string el = "";
-        int number = 0;
-        for (auto iter = expr.begin(); iter != expr.end(); ++iter) {
-            if (*iter != ' ')
-                el += *iter;
-            if (*iter == ' ' || (iter + 1) == expr.end()) {
-                number = atoi(el.c_str());
-                if (number == 0 && el != "0") {
-                    if (el.size() != 1)
-                        throw std::logic_error("It's not an operation");
-                    IOperation *oper = getOperation(el.c_str()[0]);
-                    switch (oper->getArity()) {
-                        case IOperation::arUno: {
-                            int first = _s.pop();
-                            _s.push(oper->operation(el.c_str()[0], first));
-                            break;
-                        }
-                        case IOperation::arDue: {
-                            int second = _s.pop();
-                            int first = _s.pop();
-                            _s.push(oper->operation(el.c_str()[0], first, second));
-                            break;
-                        }
-                        case IOperation::arTre: {
-                            int third = _s.pop();
-                            int second = _s.pop();
-                            int first = _s.pop();
-                            _s.push(oper->operation(el.c_str()[0], first, second, third));
-                            break;
-                        }
-                        default: {
-                            throw std::logic_error("An operation not found");
-                        }
+int StackMachine::calculate(const std::string& expr, bool clearStack)
+{
+    if(clearStack)
+        _s.clear();
+    std::string el = "";
+    int number = 0;
+    for (std::string::const_iterator iter = expr.begin(); iter != expr.end(); ++iter) {
+        if (*iter != ' ')
+            el += *iter;
+        if (*iter == ' ' || (iter + 1) == expr.end()) {
+            number = atoi(el.c_str());
+            if (number == 0 && el != "0") {
+                if (el.size() != 1)
+                    throw std::logic_error("It's not an operation");
+                IOperation *oper = getOperation(el.c_str()[0]);
+                switch (oper->getArity()) {
+                    case IOperation::arUno: {
+                        int first = _s.pop();
+                        _s.push(oper->operation(el.c_str()[0], first));
+                        break;
                     }
-                } else _s.push(number);
-                el = "";
-            }
+                    case IOperation::arDue: {
+                        int second = _s.pop();
+                        int first = _s.pop();
+                        _s.push(oper->operation(el.c_str()[0], first, second));
+                        break;
+                    }
+                    case IOperation::arTre: {
+                        int third = _s.pop();
+                        int second = _s.pop();
+                        int first = _s.pop();
+                        _s.push(oper->operation(el.c_str()[0], first, second, third));
+                        break;
+                    }
+                    default: {
+                        throw std::logic_error("An operation not found");
+                    }
+                }
+            } else _s.push(number);
+            el = "";
         }
-        return _s.top();
     }
+    return _s.top();
+}
 
-    int MinusOp::operation(char op, int a, int b, int /*c*/)
-    {
-        if (op != '-')
-            throw std::logic_error("Operation other than Minus (-) are not supported");
+int MinusOp::operation(char op, int a, int b, int /*c*/)
+{
+    if (op != '-')
+        throw std::logic_error("Operation other than Minus (-) are not supported");
 
-        return a - b;
+    return a - b;
+}
+
+IOperation::Arity MinusOp::getArity() const
+{
+    return arDue;
+}
+
+int ChoiceOp::operation(char op, int a, int b, int c)
+{
+    if (op != '?')
+        throw std::logic_error("Operation other than Choice (?) are not supported");
+
+    return a ? b : c;
+}
+
+IOperation::Arity ChoiceOp::getArity() const
+{
+    return arTre;
+}
+
+int SigChangeOp::operation(char op, int a, int /*b*/, int /*c*/)
+{
+    if (op != '!')
+        throw std::logic_error("Operation other than SigChange (!) are not supported");
+
+    return -a;
+}
+
+IOperation::Arity SigChangeOp::getArity() const
+{
+    return arUno;
+}
+
+int InverOp::operation(char op, int a, int /*b*/, int /*c*/)
+{
+    if (op != '~')
+        throw std::logic_error("Operation other than Inver (~) are not supported");
+
+    return ~a;
+}
+
+IOperation::Arity InverOp::getArity() const
+{
+    return arUno;
+}
+
+int AndOp::operation(char op, int a, int b, int /*c*/)
+{
+    if (op != '&')
+        throw std::logic_error("Operation other than And (&) are not supported");
+
+    return a & b;
+}
+
+IOperation::Arity AndOp::getArity() const
+{
+    return arDue;
+}
+
+int PowOp::operation(char op, int a, int b, int /*c*/)
+{
+    if (op != '^')
+        throw std::logic_error("Operation other than Pow (^) are not supported");
+
+    int ans = 1;
+    while (b) {
+        if (b & 1)
+            ans *= a;
+        a *= a;
+        b >>= 1;
     }
+    return ans;
+}
 
-    IOperation::Arity MinusOp::getArity() const
-    {
-        return arDue;
-    }
+IOperation::Arity PowOp::getArity() const
+{
+    return arDue;
+}
 
-    int ChoiceOp::operation(char op, int a, int b, int c)
-    {
-        if (op != '?')
-            throw std::logic_error("Operation other than Choice (?) are not supported");
+int AssignOp::operation(char op, int a, int /*b*/, int /*c*/)
+{
+    if (op != '=')
+        throw std::logic_error("Operation other than Assign (=) are not supported");
 
-        return a ? b : c;
-    }
+    return a;
+}
 
-    IOperation::Arity ChoiceOp::getArity() const
-    {
-        return arTre;
-    }
-
-    int SigChangeOp::operation(char op, int a, int /*b*/, int /*c*/)
-    {
-        if (op != '!')
-            throw std::logic_error("Operation other than SigChange (!) are not supported");
-
-        return -a;
-    }
-
-    IOperation::Arity SigChangeOp::getArity() const
-    {
-        return arUno;
-    }
-
-    int InverOp::operation(char op, int a, int /*b*/, int /*c*/)
-    {
-        if (op != '~')
-            throw std::logic_error("Operation other than Inver (~) are not supported");
-
-        return ~a;
-    }
-
-    IOperation::Arity InverOp::getArity() const
-    {
-        return arUno;
-    }
-
-    int AndOp::operation(char op, int a, int b, int /*c*/)
-    {
-        if (op != '&')
-            throw std::logic_error("Operation other than And (&) are not supported");
-
-        return a & b;
-    }
-
-    IOperation::Arity AndOp::getArity() const
-    {
-        return arDue;
-    }
-
-    int PowOp::operation(char op, int a, int b, int /*c*/)
-    {
-        if (op != '^')
-            throw std::logic_error("Operation other than Pow (^) are not supported");
-
-        int ans = 1;
-        while (b) {
-            if (b & 1)
-                ans *= a;
-            a *= a;
-            b >>= 1;
-        }
-        return ans;
-    }
-
-    IOperation::Arity PowOp::getArity() const
-    {
-        return arDue;
-    }
-
-    int AssignOp::operation(char op, int a, int /*b*/, int /*c*/)
-    {
-        if (op != '=')
-            throw std::logic_error("Operation other than Assign (=) are not supported");
-
-        return a;
-    }
-
-    IOperation::Arity AssignOp::getArity() const
-    {
-        return arUno;
-    }
+IOperation::Arity AssignOp::getArity() const
+{
+    return arUno;
+}
 
 } // namespace xi
