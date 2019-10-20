5bf2b974e1ba002006f3e9da209b6edba1defbc4
Yaroslav Riabtsev <44510108+yaryabtsev@users.noreply.github.com>
2019-10-20 15:31:54 +0300

    Update int_stack.cpp

--- src/int_stack.cpp
+++ src/int_stack.cpp
@@ -11,13 +11,58 @@
 
 
 #include "int_stack.h"
+#include <stdexcept>
 
 // TODO: add necessary headers here
 // #include <...
 
 namespace xi {
 
-// TODO: add method definitions here
+    IntStack::IntStack(size_t sz) : _ssize(sz), _head(0) 
+    {
+        _stack = new int[sz];
+    }
 
+    IntStack::~IntStack() 
+    {
+        delete[] _stack;
+        _stack = nullptr;
+    }
+
+    void IntStack::push(int el) 
+    {
+        if (_head == _ssize)
+            throw std::logic_error("int_stack overflow, can't push more elements");
+        _stack[_head++] = el;
+    }
+
+    int IntStack::top() 
+    {
+        if (_head == 0)
+            throw std::logic_error("int_stack error, stack is empty");
+        return _stack[_head - 1];
+    }
+
+    int IntStack::pop() 
+    {
+        if (_head == 0)
+            throw std::logic_error("int_stack error, stack is empty");
+        return _stack[--_head];
+    }
+
+    void IntStack::clear() 
+    {
+        _head = 0;
+    }
+
+    bool IntStack::isEmpty() const 
+    {
+        return _head == 0;
+    }
+
+    bool IntStack::isFull() const 
+    {
+        return _head == _ssize;
+    }
 } // namespace xi
 
